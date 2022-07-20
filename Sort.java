422a34de8c750fb1d05e44e23140c07a3c4e6a8b
Soumik Chatterjee <souchatt@outlook.in>
2022-07-20 20:30:46 +0530

    Add questions for Sort

---
+++ Sort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Sort {
+    public static void bubbleSort(int[] arr) {
+
+    }
+    
+    public static void selectionSort(int[] arr) {
+        
+    }
+
+    public static void insertionSort(int[] arr) {
+        
+    }
+
+    public static void mergeSort(int[] arr) {
+        
+    }
+
+    public static void quickSort(int[] arr) {
+        
+    }
+
+    private static class Entry {
+        int val;
+
+        public Entry(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void customComparatorSort(Entry[] entries) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
